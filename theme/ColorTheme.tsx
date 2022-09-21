e8960d07d8c124229c4a1f9bf1fa0e10a26c56ca
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2022-09-21 00:30:22 -0300

    criando ambiente base de cores

---
+++ theme/ColorTheme.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { NativeBaseProvider, extendTheme } from 'native-base';
+
+
+    export const viaLacteaTheme = extendTheme({
+        colors:{
+            viaLacteaPrimary: {
+                "50": "#cac4f7",
+                "100": "#a79eed",
+                "200": "#8376e5",
+                "300": "#7365d9",
+                "400": "#6456cc"
+            }
+        }
+    })
