f21db144b0030b21e8f8fae2344446367d1f3d99
julio.treichel <julio.tre@outlook.com.br>
2022-09-29 21:47:51 -0300

    commitando as alterações do Eslint e Prettier

--- src/components/FazendaFormComponent.tsx
+++ src/components/FazendaFormComponent.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import {
-    Text,
-    Box,
-    Center,
-    FormControl,
-    Input,
-    NativeBaseProvider,
-    Stack,
-    Button
+  Text,
+  Box,
+  Center,
+  FormControl,
+  Input,
+  NativeBaseProvider,
+  Stack,
+  Button,
 } from 'native-base';
 import { viaLacteaTheme } from '../config/theme/ColorTheme';
 import InputComponent from './InputComponent';
 import EnderecoFormComponent from './EnderecoFormComponent';
 
 const validate = () => {
-    //todo
+  //todo
 };
 
 export default function FazendaForm() {
-    const [fazenda, setFazenda] = React.useState({});
+  const [fazenda, setFazenda] = React.useState({});
 
-    return (
-        <NativeBaseProvider theme={viaLacteaTheme}>
-            <Center px="8%" pt="8%" justifyContent={'space-between'}>
-                <FormControl isRequired>
-                    <FormControl.Label>Nome da Fazenda</FormControl.Label>
-                    <Input
-                        placeholder="Fazenda São Jorge"
-                        onChangeText={(value: any) => {
-                            setFazenda({ ...fazenda, nome: value });
-                        }}
-                    ></Input>
-                </FormControl>
-            </Center>
-            <EnderecoFormComponent
-                onSubmit={(endereco: Object) => {
-                    setFazenda({ ...fazenda, endereco });
-                }}
-            ></EnderecoFormComponent>
-        </NativeBaseProvider>
-    );
+  return (
+    <NativeBaseProvider theme={viaLacteaTheme}>
+      <Center px="8%" pt="8%" justifyContent={'space-between'}>
+        <FormControl isRequired>
+          <FormControl.Label>Nome da Fazenda</FormControl.Label>
+          <Input
+            placeholder="Fazenda São Jorge"
+            onChangeText={(value: any) => {
+              setFazenda({ ...fazenda, nome: value });
+            }}
+          ></Input>
+        </FormControl>
+      </Center>
+      <EnderecoFormComponent
+        onSubmit={(endereco: object) => {
+          setFazenda({ ...fazenda, endereco });
+        }}
+      ></EnderecoFormComponent>
+    </NativeBaseProvider>
+  );
 }
