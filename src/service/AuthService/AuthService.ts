7e6b8ce91a57883fc351cc0519bb534ff701cf5f
willian.melo <willian.melo@elotech.com.br>
2022-11-02 15:35:48 -0300

    Fazendo com que token auth seja passado no header da requisicao

--- src/service/AuthService/AuthService.ts
+++ src/service/AuthService/AuthService.ts
@@ -1,20 +1,18 @@
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import { Login } from "../../types/Login";
 import api from "../api";
 
 const AuthService = {
 
   login : function(login:Login) {
-      api.post(`/api/auth/signin`, login)
+      axios.post(`http://localhost:8080/api/auth/signin`, login)
       .then((response:any) => {
         AsyncStorage.setItem('Token', response.data.token)
+        api.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
       })
   },
-
-
-
-
 };
 
 export default AuthService;
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
-import { Storage } from "../utils";
-
-const token = localStorage.getItem('token')
-
-const authToken:any = token != null ? `Bearer ${token}` : null;
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
-  headers: { Authorization: authToken }
 });
 
 export default api;
