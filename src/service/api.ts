2a6152f65331c8a471b6033c14134dde7d23b6a3
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2022-11-06 20:03:08 -0300

    arrumando solicitações em code review

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
-  headers: { Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY2NzAwMDYyNiwiaWF0IjoxNjY2OTE0MjI2fQ.RBNHkatOdHW7Hh9DE5bOCUntQD6NxvJSB2_TcM8lGGs5AGqn97zVazjJB9jhN3-qeCfobYlg_FyGfGBfmzf2hg` }
 });
 
 export default api;
