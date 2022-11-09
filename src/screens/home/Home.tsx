235e250c87293afcf41ba810b13ad6959d221a04
julio.treichel <julio.tre@outlook.com.br>
2022-11-09 19:32:38 -0300

    feat: criando tela Home

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,105 +1,36 @@
-import { NativeBaseProvider, Button, ScrollView } from 'native-base';
-import React, { FunctionComponent } from 'react';
+import { NativeBaseProvider, Button, ScrollView, VStack } from 'native-base';
+import React, { FunctionComponent, useState } from 'react';
 import { viaLacteaTheme } from '../../config/theme/ColorTheme';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { ReciboDeVenda } from '../../types/ReciboDeVenda';
+import ReciboDeVendaCard from '../ReciboDeVenda/components/ReciboDeVendaCardComponent';
 
 interface Props {}
 
-export type RootStackParamList = {
-  UsuarioForm: { id: string };
-  FazendaForm: { id: string };
-  FinalizarCadastro: { id: string };
-  ColetorForm: { id: string };
-  ReciboDeVendaList: { id: string };
-  CompradorForm: { id: string };
-};
+// export type RootStackParamList = {
+//   UsuarioForm: { id: string };
+//   FazendaForm: { id: string };
+//   FinalizarCadastro: { id: string };
+//   ColetorForm: { id: string };
+//   ReciboDeVendaList: { id: string };
+//   CompradorForm: { id: string };
+// };
 
 const Home: FunctionComponent<Props> = (props) => {
+  const [listaRecibo, setReciboLista] = useState<Array<any>>([]);
+  const [refreshList, setRefreshList] = useState(true);
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
     <NativeBaseProvider theme={viaLacteaTheme}>
       <ScrollView>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() =>
-            navigation.navigate('UsuarioForm', { id: 'UsuarioForm' })
-          }
-        >
-          {' '}
-          Formulario Usuario{' '}
-        </Button>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() =>
-            navigation.navigate('FazendaForm', { id: 'FazendaForm' })
-          }
-        >
-          {' '}
-          Formulario Fazenda{' '}
-        </Button>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() =>
-            navigation.navigate('FinalizarCadastro', {
-              id: 'FinalizarCadastro',
-            })
-          }
-        >
-          {' '}
-          Finalizar Cadastro{' '}
-        </Button>
-
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() => navigation.navigate('ReciboDeVendaList', {})}
-        >
-          {' '}
-          Recibo de Venda{' '}
-        </Button>
-
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() => navigation.navigate('UsuarioForm', { id: 'Novo' })}
-        >
-          {' '}
-          Formulario Usuario{' '}
-        </Button>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() => navigation.navigate('FazendaForm', { id: 'Novo' })}
-        >
-          {' '}
-          Formulario Fazenda{' '}
-        </Button>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() =>
-            navigation.navigate('FinalizarCadastro', {
-              id: 'FinalizarCadastro',
-            })
-          }
-        >
-          {' '}
-          Finalizar Cadastro{' '}
-        </Button>
-        <Button
-          m="5"
-          bg={'viaLacteaSecondary.blue'}
-          onPress={() => navigation.navigate('CompradorForm', { id: 'Novo' })}
-        >
-          {' '}
-          Cadastrar Comprador{' '}
-        </Button>
+      <VStack space={4}>
+          {listaRecibo.map((item: ReciboDeVenda) => {
+            return <ReciboDeVendaCard recibo={item}> </ReciboDeVendaCard>;
+          })}
+        </VStack>
       </ScrollView>
     </NativeBaseProvider>
   );
